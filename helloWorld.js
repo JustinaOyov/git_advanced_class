125def59ced16a1af0c5dd5408b07c93bcbd153a
justinaoyov <justina.oyovwevotu@gmail.com>
2019-09-01 15:23:13 +0200

    added new function

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,7 @@
 const helloWorld = function() {
   return 'Hello world!';
 };
-const byeworld = function()
+const goodmorning = function()
 console.log(helloWorld())
 console.log("Hello again");
 console.log("potatoes");
